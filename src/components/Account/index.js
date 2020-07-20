dcab3017ca2e5df50d301a19024c34b973cbd96b
MattDevy <mattd905@gmail.com>
2020-07-21 00:04:27 +0100

    Fix login issues

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,5 +1,14 @@
-import React from 'react'
-
-const AccountPage = () => <div>Account</div>
-
-export default AccountPage
+import React from 'react';
+ 
+import { PasswordForgetForm } from '../PasswordForget';
+import PasswordChangeForm from '../PasswordChange';
+ 
+const AccountPage = () => (
+  <div>
+    <h1>Account Page</h1>
+    <PasswordForgetForm />
+    <PasswordChangeForm />
+  </div>
+);
+ 
+export default AccountPage;
